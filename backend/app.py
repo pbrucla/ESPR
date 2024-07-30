17755065d075633bef32683aeb334fb8673c1e3c
Joshua Zhu <23546968+joshuazhu17@users.noreply.github.com>
2024-07-29 17:12:18 -0700

    Sample individual package endpoint in backend

--- backend/app.py
+++ backend/app.py
@@ -34,3 +34,32 @@ def publish_package_sample():
   description = request.form['description']
 
   return {"name": name, "initial_version": initial_version, "description": description}, 201
+
+
+@app.route("/package_info_sample/0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266", methods=['GET'])
+def package_info_sample_1():
+  info = {
+    "name": "yahaha",
+    "author": "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
+    "versions": [
+      "1.0.0",
+      "1.0.1",
+      "1.0.2",
+      "1.1.0",
+      "2.0.0"
+    ],
+    "dependencies": {
+      "1.0.0": ["numpy >= 1.20.0"],
+      "1.0.1": ["numpy >= 1.20.0"],
+      "1.0.2": ["numpy >= 1.23.0"],
+      "1.1.0": ["numpy >= 1.24.0"],
+      "2.0.0": ["numpy >= 2.0.0", "web3 >= 6.20.0"]
+    },
+    "collaborators": [
+      "0x70997970C51812dc3A010C7d01b50e0d17dc79C8",
+      "0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC"
+    ],
+    "status": "alive"
+  }
+
+  return info, 200
